f4de1f71b1d624bee871b08ec2175752acb1cb26
Charlie Tysse <tyssejc@gmail.com>
2024-04-18 08:59:59 -0400

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+import netlify from "@astrojs/netlify";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://ctysse.net',
+  integrations: [mdx(), sitemap()],
+  output: "server",
+  adapter: netlify()
+});
