000df122294379e765e8b030cdd8c1625d2b514d
Charlie Tysse <tyssejc@gmail.com>
2024-04-18 12:06:15 -0400

    config, social icons, about, 1st post

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Charlie Tysse';
+export const SITE_DESCRIPTION = 'Analytics implementation and mental health';
