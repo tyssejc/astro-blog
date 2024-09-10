1e93d1c01142d7455af0812c23cd823f04fa53b9
Charlie Tysse <tyssejc@gmail.com>
2024-09-10 12:50:39 -0400

    change adapter to cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import netlify from "@astrojs/netlify";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ctysse.net',
   integrations: [mdx(), sitemap()],
   output: "server",
-  adapter: netlify()
+  adapter: cloudflare()
 });
